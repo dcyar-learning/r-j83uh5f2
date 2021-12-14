95f942d2de52da62d3dd1b59dead7ff695b6870f
Jhon Achata Limachi <jhonal183@gmail.com>
2021-12-14 07:02:05 -0500

    run run run

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleInputChange = ({ target }) => this.setState.text(target.value)
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
+        <textarea rows="3" onChange={handleInputChange} value={this.state.text}></textarea>
         <div className="counter">0</div>
       </div>
     );
